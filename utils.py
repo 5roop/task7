f55e6791f633ee8fe4200fba60036564d24e5298
5roop <rupnikpeter@gmail.com>
2022-02-24 11:26:26 +0100

    Add lxml to BS

--- utils.py
+++ utils.py
@@ -365,7 +365,7 @@ def read_prevert(file: str):
         filecontent = "<data>" + f.read() + "</data>"
 
     # Passing the stored data inside the beautifulsoup parser 
-    data = BeautifulSoup(filecontent,)
+    data = BeautifulSoup(filecontent,"lxml")
     docs = data.find_all("doc")
     parsed_docs = list()
 
